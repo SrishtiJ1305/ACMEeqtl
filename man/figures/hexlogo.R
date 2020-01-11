e7afb2769d670542aae5efaee2302bd2c82a3390
SrishtiJ1305 <59254025+SrishtiJ1305@users.noreply.github.com>
2020-01-11 19:54:28 +0100

    Update hexlogo.R

--- man/figures/hexlogo.R
+++ man/figures/hexlogo.R
@@ -2,8 +2,8 @@ library(hexSticker)
 
 sticker(~plot(cars, cex=.5, cex.axis=.5, type="l", mgp=c(0,.3,0), xlab="", ylab=""),
         package="ACMEeqtl", p_size=8, s_x=.8, s_y=.6, s_width=1.4, s_height=1.2,
-        filename="C:\\Users\\srish\\Desktop\\logo.svg")
+        filename="man\\figures\\logo.svg")
 
 sticker(~plot(cars, cex=.5, cex.axis=.5, type="l",mgp=c(0,.3,0), xlab="", ylab=""),
         package="ACMEeqtl", p_size=8, s_x=.8, s_y=.6, s_width=1.4, s_height=1.2,
-        filename="C:\\Users\\srish\\Desktop\\logo.png")
+        filename="man\\figures\\logo.png")
