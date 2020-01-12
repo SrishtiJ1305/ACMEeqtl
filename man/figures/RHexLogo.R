bccdfe3caa32fb5d900a9d2e2ea3ce9dd73a1310
SrishtiJ1305 <SrishtiJ1305@users.noreply.github.com>
2020-01-12 14:48:44 +0100

    LogoFinal

---
+++ man/figures/RHexLogo.R
@@ -0,0 +1,43 @@
+library(hexSticker)
+library("ggplot2")
+
+#This is a copied code from the ACMEeqtl Package on GitHub I exported this plot as a png file (Rplotforlogo.png) and svg file (PlotForLogo.svg)and added it to my HexLogo
+logo= {StatLm <- ggproto("StatLm", Stat, 
+                  required_aes = c("x", "y"),
+                  
+                  compute_group = function(data, scales) {
+                    rng <- range(data$x, na.rm = TRUE)
+                    grid <- data.frame(x = rng)
+                    
+                    mod <- lm(y ~ x, data = data)
+                    grid$y <- predict(mod, newdata = grid)
+                    
+                    grid
+                  }
+)
+
+stat_lm <- function(mapping = NULL, data = NULL, geom = "line",
+                    position = "identity", na.rm = FALSE, show.legend = NA, 
+                    inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatLm, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+ggplot(mpg, aes(displ, hwy)) + 
+  geom_point() + 
+  stat_lm()}
+
+
+#This is the code for my HexLogo 
+
+imgurl=system.file("C:\\Users\\srish\\Desktop\\Rplotforlogo.png", package="hexsticker")
+sticker("C:\\Users\\srish\\Desktop\\Rplotforlogo.png", package="ACMEeqtl", p_size=15, s_x=1, s_y=.8, s_width=.6,
+        filename="logo.png")
+
+imgurl=system.file("C:\\Users\\srish\\Desktop\\PlotForLogo.svg", package="hexsticker")
+sticker("C:\\Users\\srish\\Desktop\\PlotForLogo.svg", package="ACMEeqtl", p_size=8, s_x=1, s_y=.8, s_width=.5,
+        filename="logo.svg")
+
